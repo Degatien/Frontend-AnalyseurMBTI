887138eb35b7fae5a8f6b517e74baf40bb9044c6
Degatien <degatien@gmail.Com>
2020-12-14 12:19:37 +0100

    [ADD] Input utilisateur pour le texte
    Ajout de React-Bootstrap pour avoir une jolie page

--- mbti/src/App.js
+++ mbti/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Button from 'react-bootstrap/Button';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [textInput, setTextInput] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [receivedAnswer, setReceivedAnswer] = useState(false);
+
+    const handleChange = (event) => setTextInput(event.target.value);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsLoading(true);
+    };
+
+    if (!receivedAnswer && !isLoading) {
+        return (
+            <div className="App">
+                <h1>Bienvenu sur notre application MBTI</h1>
+                <h2>Veuillez entrer un texte ci-dessous. Notre application se chargera de déterminer votre type MBTI</h2>
+                <form onSubmit={handleSubmit}>
+                    <textarea className="Text-Input" type="text" value={textInput} onChange={handleChange} />
+                    <br/>
+                    <Button onClick={handleSubmit} variant="light" size="lg">Envoyer</Button>
+                </form>
+            </div>
+        );
+    } else if (receivedAnswer && isLoading) {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    } else if (receivedAnswer && !isLoading) {
+        return (
+            <div className="App">
+
+            </div>
+        );
+    }
+};
 
 export default App;
